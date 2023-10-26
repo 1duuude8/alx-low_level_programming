c0434da3ae9b7e793e63157c54f47a0b7fd3ba16
1duuude8 <younesarab3@gmail.com>
2023-10-26 11:58:14 -0700

    task of project 0x14

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+/**
+ * get_endianness - check if  machine is little or big endian
+ * Return: 0 big, 1 little
+ */
+int get_endianness(void)
+{
+
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+tasks of project  0x14 bit manipulation
